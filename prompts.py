435b882e2e42c49b9138cf645ea30232299dc34e
Sydney Nguyen <melosydrose@gmail.com>
2023-10-30 21:32:52 -0400

    refactor prompt

---
+++ prompts.py
@@ -0,0 +1,5 @@
+TRAITS_PROMPT = """
+                        Given a diagnosis of a species, create a vertical numbered list of all its traits.
+                        Diagnosis: {diagnosis}
+                        Traits: These are the traits for the above species diagnosis:
+                """
