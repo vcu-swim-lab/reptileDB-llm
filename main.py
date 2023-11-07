9b944f3e8cca286c1f8a8a683f0ba5686b8456ea
Sydney Nguyen <melosydrose@gmail.com>
2023-11-07 06:06:51 -0500

    Improve prompt and write traits to csv file with categories

--- main.py
+++ main.py
@@ -10,27 +10,40 @@ load_dotenv()
 API_KEY = os.environ['OPENAI_API_KEY']
 
 if __name__ == "__main__":
-    with open("Numbered reptile descriptions.txt", encoding="utf-8") as file:
-        for line in file:
-            diagnosis_index = line.find("Diagnosis")
+    with open("Numbered reptile descriptions.txt.txt", encoding="utf-8") as file:
+        with open('extracted_traits.csv', mode='w', newline='', encoding='utf-8') as csvfile:
+            fieldnames = ['Species']
 
-            if diagnosis_index != -1:
-                species = line[:diagnosis_index].strip().replace("$", "")
-                diagnosis = line[diagnosis_index + len("Diagnosis"):].strip()
+            for line in file:
+                diagnosis_index = line.find("Diagnosis")
 
-                if species:
-                    categorized_traits = traits.get_categorized_traits().run(f"{species}: {diagnosis}")
+                if diagnosis_index != -1:
+                    species = line[:diagnosis_index].strip().replace("$", "")
+                    diagnosis = line[diagnosis_index + len("Diagnosis"):].strip()
 
-                    match = re.match(r"([\w\s-]+): (.+)", categorized_traits)
+                    if species:
+                        categorized_traits = traits.get_categorized_traits().run(f"{species}: {diagnosis}")
+                        print(categorized_traits)
+                        match = re.match(r"([\w\s-]+): (.+)", categorized_traits)
 
-                    if match:
-                        species_name = match.group(1)
-                        traits_info = match.group(2)
+                        if match:
+                            species_name = species
+                            traits_info = match.group(2)
 
-                        characteristics = re.findall(r"([\w\s-]+) <([\w\s-]+)>", traits_info)
+                            characteristics = re.findall(r"([\w\s-]+) <([\w\s-]+)>", traits_info)
 
-                        print("Species:", species_name)
-                        for characteristic, trait_category in characteristics:
-                            print("Characteristic:", characteristic)
-                            print("Trait Category:", trait_category)
+                            for characteristic, trait_category in characteristics:
+                                if trait_category not in fieldnames:
+                                    fieldnames.append(trait_category)
 
+                            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+                            if csvfile.tell() == 0:
+                                writer.writeheader()
+
+                            row_data = {'Species': species_name}
+
+                            for characteristic, trait_category in characteristics:
+                                row_data[trait_category] = characteristic
+
+                            writer.writerow(row_data)
--- prompts.py
+++ prompts.py
@@ -5,13 +5,18 @@ EXTRACT_TRAITS_PROMPT = """
 
 CATEGORIES_PROMPT = """
                     Given the {characteristics} of a species, categorize each into trait categories. 
-                    
+
                     Provide the ideal output in the format: (species): (characteristic) <(trait category)>, 
                     (characteristic) <(trait category)>, ...
-                    
+
                     Ensure that the trait category is in angled brackets <>
-                    
-                    Here are some examples of trait categories: midbody scale, rows, ventrals, body bands, nape band,
-                    subocular scale, prefrontal-orbit, contact, supralabials, infralabials, subcaudals, 
-                    ventral blotches, prefrontal lateral blotches, SVL, dorsal scales, anal, loreal, and more.                                            
-                  """
+
+                    Here is an example:
+                    Characteristics: "Cylindrophis ruffus; Characteristics: 19 midbody 
+                    scale rows, 186–197 ventrals, wide and constant bands encircling dark body, an interrupted and 
+                    wide band on the nape. "
+
+                    Ideal Output: "Cylindrophis ruffus: 19 midbody scale rows <midbody scale rows>, 
+                    186-197 ventrals <ventrals>, wide and constant bands encircling dark body <body bands>, 
+                    interrupted and wide band on the nape <nape band>"
+                    """
