d11c7802ea4ef0e29bb08289e587bfbf80d9cfd6
Sydney Nguyen <melosydrose@gmail.com>
2023-11-10 03:23:22 -0500

    Update data

---
+++ prompts/prompts_v2.py
@@ -0,0 +1,22 @@
+EXTRACT_TRAITS_PROMPT_V2 = """
+                        Given {diagnosis}, identify the species name and extract its characteristics.
+                        Format the output like this: "species name: characteristics"
+                """
+
+CATEGORIES_PROMPT_V2 = """
+                    Classify the {characteristics} into trait categories. 
+
+                    Provide the ideal output in the format: (species): (characteristic) <(trait category)>, 
+                    (characteristic) <(trait category)>, ...
+
+                    Ensure that the trait category is in angled brackets <>
+
+                    Here is an example:
+                    Characteristics: "Cylindrophis ruffus; Characteristics: 19 midbody 
+                    scale rows, 186–197 ventrals, wide and constant bands encircling dark body, an interrupted and 
+                    wide band on the nape. "
+
+                    Ideal Output: "Cylindrophis ruffus: 19 midbody scale rows <midbody scale rows>, 
+                    186-197 ventrals <ventrals>, wide and constant bands encircling dark body <body bands>, 
+                    interrupted and wide band on the nape <nape band>"
+                    """
